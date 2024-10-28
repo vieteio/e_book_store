323fd2a725113593d2fbac77085828d1535392f9
Stanislav Kapulkin <kapulkin@gmail.com>
2024-10-28 05:56:06 +0200

    empty repo

--- content_ingestion/services/analytics_service.py
+++
@@ -1,6 +0,0 @@
-# import pandas as pd
-
-class AnalyticsService:
-    def update_ingestion_analytics(self, book_id: str, ingestion_data: dict) -> dict:
-        # TODO: Implement actual analytics update
-        return {"update_status": "success"}
--- content_ingestion/services/drm_service.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-class DRMService:
-    def __init__(self):
-        self.kms = boto3.client('kms', region_name='us-east-1')
-
-    def apply_drm(self, file_path: str) -> str:
-        # TODO: Implement actual DRM application
-        return "path/to/protected/file.epub"
